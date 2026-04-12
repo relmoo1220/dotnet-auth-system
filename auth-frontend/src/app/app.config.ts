2913042a56f1cb807fda9e20d8343c76a133dd8c
relmoo1220 <gamingwithmura@gmail.com>
2026-04-12 18:22:11 +0800

    feat: login page

--- auth-frontend/src/app/app.config.ts
+++ auth-frontend/src/app/app.config.ts
@@ -4,9 +4,18 @@ import { provideRouter } from '@angular/router';
 import { routes } from './app.routes';
 import { provideClientHydration, withEventReplay } from '@angular/platform-browser';
 
+import { providePrimeNG } from 'primeng/config';
+import Aura from '@primeuix/themes/aura';
+
 export const appConfig: ApplicationConfig = {
   providers: [
+    providePrimeNG({
+      theme: {
+        preset: Aura,
+      },
+    }),
     provideBrowserGlobalErrorListeners(),
-    provideRouter(routes), provideClientHydration(withEventReplay())
-  ]
+    provideRouter(routes),
+    provideClientHydration(withEventReplay()),
+  ],
 };
--- auth-frontend/src/app/app.routes.ts
+++ auth-frontend/src/app/app.routes.ts
@@ -1,3 +1,7 @@
 import { Routes } from '@angular/router';
+import { authRoutes } from './features/auth/auth.routes';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: 'auth', children: authRoutes },
+  { path: '', redirectTo: 'auth/login', pathMatch: 'full' },
+];
